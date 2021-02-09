4b586fc198fbcf4fde849dec56ae4bbc17a1333b
Le Xuan Du <dulx96@gmail.com>
2021-02-09 13:46:38 +0900

    [release] release v0.2.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 setup(
     name='bb_logger',
     packages=['bb_logger'],
-    version='0.2.3',
+    version='0.2.4',
     # Chose a license from here:
     # https://help.github.com/articles/licensing-a-repository
     license='MIT',
